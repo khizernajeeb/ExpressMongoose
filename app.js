721f5740aa636680858048c5ba1546cc98f264d7
Khizer Najeeb <khizernajeeb@gmail.com>
2018-01-08 12:43:12 +0500

    change app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
-// Initiale app object
+// Initiale app object 2
 var app = express();
 
 var mongoose = require('mongoose');
